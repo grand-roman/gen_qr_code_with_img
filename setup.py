1488f8f69d4ab5a1c8f6c8078ffea6166d38d4fa
Roman <31464179+grand-roman@users.noreply.github.com>
2023-08-28 13:10:50 +0700

    fix

--- setup.py
+++ setup.py
@@ -7,14 +7,15 @@ requirements = ["path>=16.3.0", "Pillow>=9.0.1", "qrcode>=7.3.1"]
 
 setuptools.setup(
     name="gen_qr_with_img",
-    version="1.0.1",
+    version="1.0.2",
     author="Roman Andreev",
     author_email="grand-roman@yandex.ru",
     description="Generation QR Code with IMG",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/grand-roman/gen_qr_code_with_img",
-    packages=setuptools.find_packages() + ['gen_qr_with_img'],
+    packages=['gen_qr_with_img'],
     install_requires=requirements,
     python_requires='>=3.6',
+    zip_safe=False,
 )
