04cc29e1e77e4a073d6b354f19457dbd0fd7fd99
Roman <31464179+grand-roman@users.noreply.github.com>
2023-08-28 13:18:04 +0700

    fix

--- example.py
+++ example.py
@@ -1,6 +1,5 @@
 from path import Path
 from qrcode_img import QRCode
-from convert import png_to_svg
 
 
 
@@ -13,13 +12,3 @@ path_to_save = Path().joinpath("example", "1example.png")
 qrcode = QRCode(text)
 
 qrcode.gen_qr_code(path_to_download, path_to_save)
-
-
-svg_pixel = png_to_svg(path_to_save)
-
-
-f = open("test.txt", "w")
-
-f.write(svg_pixel)
-
-f.close()
